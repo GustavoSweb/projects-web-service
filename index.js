f86172379ac2120531451bf0de5298cb48a432e0
Gustavo <gustavodasilvama10@gmail.com>
2024-05-11 10:08:41 -0300

    Home api inicialize

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const database = require('./database/connection')
-const ProjectsRouter = require("./routes/Projects")
-const SkillsRouter = require("./routes/Skills")
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const database = require("./database/connection");
+const ProjectsRouter = require("./routes/Projects");
+const SkillsRouter = require("./routes/Skills");
 
-var cors = require('cors')
+var cors = require("cors");
 
-app.use(cors())
- 
-app.use('/', ProjectsRouter)
-app.use('/', SkillsRouter)
+app.use(cors());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use("/", ProjectsRouter);
+app.use("/", SkillsRouter);
+app.get("/", (req, res) => {
+  res.send("API FUNCIONANDO");
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 8081
-app.listen(PORT, ()=>{
-    console.log('Servidor Rodando...')
-})
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log("Servidor Rodando...");
+});
